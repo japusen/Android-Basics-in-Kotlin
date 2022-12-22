5eb2113385c42f28ff0cb12477878cd863c92acc
Jay <japusen32@gmail.com>
2022-12-22 01:02:34 -0800

    Affirmations - Display a list of images using cards

---
+++ Affirmations/app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,11 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceID: Int,
+    @DrawableRes val imageResourceId: Int
+) {
+
+}
